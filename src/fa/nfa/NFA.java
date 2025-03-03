e1f990d0bd5ccb2d0534fb080ad38b9d83c84b00
spencer <spencerford674@u.boisestate.edu>
2025-03-03 12:24:29 -0700

    Initial Commit

---
+++ src/fa/nfa/NFA.java
@@ -0,0 +1,84 @@
+package fa.nfa;
+
+import fa.State;
+
+import java.util.Set;
+
+public class NFA implements NFAInterface {
+
+
+
+    public NFA(String name) {
+
+    }
+
+    @Override
+    public Set<NFAState> getToState(NFAState from, char onSymb) {
+        return Set.of();
+    }
+
+    @Override
+    public Set<NFAState> eClosure(NFAState s) {
+        return Set.of();
+    }
+
+    @Override
+    public int maxCopies(String s) {
+        return 0;
+    }
+
+    @Override
+    public boolean addTransition(String fromState, Set<String> toStates, char onSymb) {
+        return false;
+    }
+
+    @Override
+    public boolean isDFA() {
+        return false;
+    }
+
+    @Override
+    public boolean addState(String name) {
+        return false;
+    }
+
+    @Override
+    public boolean setFinal(String name) {
+        return false;
+    }
+
+    @Override
+    public boolean setStart(String name) {
+        return false;
+    }
+
+    @Override
+    public void addSigma(char symbol) {
+
+    }
+
+    @Override
+    public boolean accepts(String s) {
+        return false;
+    }
+
+    @Override
+    public Set<Character> getSigma() {
+        return Set.of();
+    }
+
+    @Override
+    public State getState(String name) {
+        return null;
+    }
+
+    @Override
+    public boolean isFinal(String name) {
+        return false;
+    }
+
+    @Override
+    public boolean isStart(String name) {
+        return false;
+    }
+}
---
+++ src/fa/nfa/NFAState.java
@@ -0,0 +1,6 @@
+package fa.nfa;
+import fa.State;
+
+public class NFAState extends State {
+
+}
