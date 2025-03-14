0e5a5f33715d564960b702fc7a8841e1dff41260
AcostaBSU <acostabsu@gmail.com>
2025-03-14 17:21:03 -0600

    Re-implement Ford' JavaDoc documentation

--- src/fa/nfa/NFA.java
+++ src/fa/nfa/NFA.java
@@ -2,6 +2,13 @@ package fa.nfa;
 
 import java.util.*;
 
+/**
+ * DFA is an implementation that provides all necessary operations to construct
+ * a Determinate Finite Automata.
+ * @author Luis Acosta
+ * @author Spencer Ford
+ */
+
 public class NFA implements NFAInterface {
 
     private LinkedHashSet<Character> sigma;
@@ -21,6 +28,12 @@ public class NFA implements NFAInterface {
         return Set.of();
     }
 
+    /** Returns Set of valid 'e' transitions from a state 's'
+     * This method creates a stack and set of valid transitions. After when the stack is empty,
+     * meaning the number of valid transitions is done the set is returned.
+     * @param s - NFAState - state to find eClosure
+     * @return - Set<NFAState> - set of valid 'e' transitions from input 's'.
+     */
     @Override
     public Set<NFAState> eClosure(NFAState s) {
         Set<NFAState> closure = new HashSet<>();
@@ -44,6 +57,15 @@ public class NFA implements NFAInterface {
         return closure;
     }
 
+    /** The max number of copies of this object were created when traversing the inputs string
+     * This method will calculate the eClosure of the startState. It will then loop through
+     * every character in the string then it will add every valid transition (including e)to a Set  of
+     * state objects and comparing the size of this Set to the previously calculated max copies for the
+     * largest.
+     * @param s - the string to be consumed and checked for acceptance by this NFA
+     * @return int - an int holding the max number of copies of this object created when traversing
+     * the input string.
+     **/
     @Override
     public int maxCopies(String s) {
         Set<NFAState> currentStates = eClosure(states.get(startState));
@@ -70,6 +92,15 @@ public class NFA implements NFAInterface {
         return maxCopies;
     }
 
+    /** Returns boolean value if specified transition is valid
+     * This method will check to make sure fromState, toState are states already in the NFA and the character
+     * is in the language. Then it will add the transition to the fromState transition set.
+     * @param fromState - state transiton is associated with
+     *          toState - state transition is going to
+     *           onSymb - character in the language the transition occurs on
+     * @return boolean - true - transition successfully added
+     *                  false - transition failed to be added
+     **/
     @Override
     public boolean addTransition(String fromState, Set<String> toStates, char onSymb) {
         if( states.containsKey(fromState) && (sigma.contains(onSymb) || onSymb == 'e')) {
@@ -85,6 +116,13 @@ public class NFA implements NFAInterface {
         return false;
     }
 
+    /** Returns boolean value if this object is a DFA
+     * This method will look through every states' transition table looking for epsilon
+     * transitions and if a specific state has more than one transition on a single
+     * character.
+     * @return boolean - true - this object is a DFA
+     *                  false - this object is an NFA
+     */
     @Override
     public boolean isDFA() {
 
@@ -108,6 +146,11 @@ public class NFA implements NFAInterface {
         return true;
     }
 
+    /** Adds States to a NFA Instance
+     * @param name - the requested name of the state to be added.
+     * @return boolean - true - state does not already exist and was successfully added
+     *                 - false - state was not added. Probably do to the state of 'name' already exists.
+     **/
     @Override
     public boolean addState(String name) {
         if ( !states.containsKey(name) ) {
@@ -119,6 +162,11 @@ public class NFA implements NFAInterface {
         return false;
     }
 
+    /** Adds existing state to a final state set
+     * @param name - the name of the state to be added to final state set
+     * @return boolean - true - specified state was successfully added to final state set
+     *                 - false - specified state not added to final state set
+     **/
     @Override
     public boolean setFinal(String name) {
         if( states.containsKey(name) ) {
@@ -128,6 +176,11 @@ public class NFA implements NFAInterface {
         return false;
     }
 
+    /** Sets existing state as the start state
+     * @param name - the name of the state to be made the start state
+     * @return boolean - true - specified state was successfully made the start state
+     *                 - false - specified state not made the start state
+     **/
     @Override
     public boolean setStart(String name) {
         if( states.containsKey(name) ) {
@@ -137,6 +190,9 @@ public class NFA implements NFAInterface {
         return false;
     }
 
+    /** Add a Character to the language
+     * @param symbol - Character to be added to the language
+     **/
     @Override
     public void addSigma(char symbol) {
         if(!sigma.contains(symbol) && symbol != 'e') {
@@ -144,6 +200,19 @@ public class NFA implements NFAInterface {
         }
     }
 
+    /** Travels the NFA object and return if the passed string is part of the language.
+     * First, this method will determine the eClosure of the start state.
+     * Next, method will break the string up in to an array of Characters to be compared
+     * to the Characters that make up the names of the states of this NFA object. For every
+     * character of the array this method will see if the state has a valid transition available.
+     * Then method will then add it to the stack of available paths per character consumption.
+     * The method will consume the string as long as valid transitions exist until the end when it
+     * will make sure the state that it ends on is a final state.
+     * @param s - the string that is being tested if it is accepted by the NFA object
+     * @return boolean - true - specified string successfully traveled the NFA and landed on a final state
+     *                 - false - specified string failed to find valid transitions to consume the input string
+     *                           or the current state at the end of the input string is not a final state
+     **/
     @Override
     public boolean accepts(String s) {
         Set<NFAState> currentStates = eClosure(states.get(startState));
@@ -174,11 +243,18 @@ public class NFA implements NFAInterface {
         return false;
     }
 
+    /** Returns Set of Characters containing the language
+     * @return sigma - Set of Characters containing the language
+     **/
     @Override
     public Set<Character> getSigma() {
         return sigma;
     }
 
+    /** Returns the NFAState object requested
+     * @param name - the name of the state to be returned
+     * @return NFAState object
+     **/
     @Override
     public NFAState getState(String name) {
         if (states.containsKey(name)) {
@@ -187,11 +263,21 @@ public class NFA implements NFAInterface {
         return null;
     }
 
+    /** Returns boolean value if specified string is a final state
+     * @param name - the name of the state to be checked for final state status
+     * @return boolean - true - state is a final state
+     *                  false - state is not a final state
+     **/
     @Override
     public boolean isFinal(String name) {
         return finalStates.contains(name);
     }
 
+    /** Returns boolean value if specified string is the start state
+     * @param name - the name of the state to be checked for start state status
+     * @return boolean - true - state is the start state
+     *                  false - state is not the start state
+     **/
     @Override
     public boolean isStart(String name) {
         return startState.contentEquals(name);
